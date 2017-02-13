48cc1365944a84ac231d082fe7e7fa18b109db59
李亮 <admin@wlitsoft.com>
2017-02-13 21:39:47 +0800

    初始化项目 实现service注解demo示例。

---
+++ src/java/service/PersonService.java
@@ -0,0 +1,7 @@
+package service;
+
+/**
+ * Created by liliang on 2017/2/13.
+ */
+public class PersonService {
+}
---
+++ src/test/java/base/TestBase.java
@@ -0,0 +1,17 @@
+
+import org.junit.runner.RunWith;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by liliang on 2017/2/13.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:ApplicationContext.xml")
+public abstract class TestBase extends AbstractJUnit4SpringContextTests {
+
+    protected static ApplicationContext appContext;
+}
